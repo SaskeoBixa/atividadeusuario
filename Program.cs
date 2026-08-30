83047164f0e677abc4c04ee7227271549a4d81ba
Saske o Bixa <Saskedrawings@gmail.com>
2026-08-30 19:59:02 -0300

    Atividade feita (alterações ainda serão feitas)

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("+------------------------------+");
+Console.WriteLine("|       Dados do Usuário       |");
+Console.WriteLine("+------------------------------+");
+Console.WriteLine("| Nome: .....                  |");
+String nome = Console.ReadLine()!;
+Console.WriteLine("+------------------------------+");
+Console.WriteLine($"Ola, {nome}, seja bem vindo(a)!");
